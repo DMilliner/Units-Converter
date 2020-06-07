3758d8964ca7f79e86fd8157db8777452bb0c840
DMilliner <david.f.milliner@gmail.com>
2020-06-07 14:42:04 -0700

    Add Bytes

--- app/src/main/java/com/morenn/converter/utils/Bytes.kt
+++ app/src/main/java/com/morenn/converter/utils/Bytes.kt
@@ -6,5 +6,18 @@ val bytesList = listOf(
 )
 
 fun convertBytes(primaryValue: Double, primaryUnit: UnitType?, secondaryUnit: UnitType?): Double {
+    if (primaryUnit != null && secondaryUnit != null) {
+        val coefficient: Double = when {
+            primaryUnit == UnitType.BIT && secondaryUnit == UnitType.BIT -> 1.0
+            primaryUnit == UnitType.DEGREE && secondaryUnit == UnitType.BYTE -> 0.125
+
+            primaryUnit == UnitType.BYTE && secondaryUnit == UnitType.BIT -> 8.0
+            primaryUnit == UnitType.BYTE && secondaryUnit == UnitType.BYTE -> 1.0
+            else -> 0.0
+        }
+
+        return primaryValue * coefficient
+    }
+
     return 0.0
 }
